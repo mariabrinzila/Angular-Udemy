3573eb47c6c85a418ef48f9cbcb41c1cf1543ef2
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-08-18 19:09:52 +0300

    ADD Project

---
+++ Project/src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+// Blueprint for how an object should look
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+}
